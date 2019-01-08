d7e5cf68a0c806bc2857789d07b2209ce0fe3c18
leifriksheim <riksheim.leif@gmail.com>
2019-01-08 15:53:34 -0600

    Avoid rerendering fixes #1

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 export default {
-  scrollTop: 10,
+  scrollTop: 0,
   start: function() {
+    // Check if body is already freezed
+    const isFreezed = document.body.getAttribute("freezed");
+    if (isFreezed) return;
+
+    // Save scroll position
     this.scrollTop =
       window.scrollY ||
       window.scrollTop ||
       document.getElementsByTagName("html")[0].scrollTop;
-    // Disable body
+
     document.body.style.position = "fixed";
     document.body.style.overflow = "hidden";
     document.body.style.top = `-${this.scrollTop}px`;
     document.body.style.width = "100vw";
+
+    document.body.setAttribute("freezed", "");
   },
   stop: function() {
-    // Save scroll position
+    // Check if body is already unfreezed
+    const isFreezed = document.body.hasAttribute("freezed");
+    if (!isFreezed) return;
+
+    // Enable scrolling again
     document.body.style.position = "relative";
-    document.body.style.top = "";
-    document.body.style.width = "";
-    document.body.style.overflow = "";
+    document.body.style.top = null;
+    document.body.style.width = null;
+    document.body.style.overflow = null;
     window.scrollTo(0, this.scrollTop);
+
+    document.body.removeAttribute("freezed");
   }
 };
